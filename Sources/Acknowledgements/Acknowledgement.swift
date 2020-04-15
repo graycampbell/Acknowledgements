a93d3748b44df831441acdbf617e62132f21b375
Gray Campbell <12163070+graycampbell@users.noreply.github.com>
2020-04-15 16:28:14 -0500

    Add Acknowledgements Acknowledgement and documentation

--- Sources/Acknowledgements/Acknowledgement.swift
+++ Sources/Acknowledgements/Acknowledgement.swift
@@ -9,19 +9,47 @@ import Foundation
 
 // MARK: Properties & Initializers
 
+/// An `Acknowledgement` that contains a name, copyright, and license.
+
 public struct Acknowledgement {
     
     // MARK: Properties
     
+    /// The name of the acknowledgement (i.e. the framework or service you are incorporating into your software).
+    
     public let name: String
+    
+    /// The copyright notice (e.g. "Copyright © 2020 Gray Campbell").
+    
     public let copyright: String
+    
+    /// The license provided by the framework or service (MIT, Apache 2.0, etc.).
+    
     public let license: License
+    
+    /// The full text of the acknowledgement (includes name, copyright, and license text).
     
     public var fullText: String {
         return "\(self.name)\n\n\(self.copyright)\n\n\(self.license.text)"
     }
     
+    /// An `Acknowledgement` for the Acknowledgements framework.
+    ///
+    /// `name = "Acknowledgements"`
+    ///
+    /// `copyright = "Copyright © 2020 Gray Campbell"`
+    ///
+    /// `license = .MIT`
+    
+    public static let acknowledgements = Acknowledgement(name: "Acknowledgements", copyright: "Copyright © 2020 Gray Campbell", license: .MIT)
+    
     // MARK: Initializers
+    
+    /// Initializes and returns a new `Acknowledgement`.
+    ///
+    /// - Parameter name: The name of the acknowledgement (i.e. the framework or service you are incorporating into your software).
+    /// - Parameter copyright: The copyright notice (e.g. "Copyright © 2020 Gray Campbell").
+    /// - Parameter license: The license provided by the framework or service (MIT, Apache 2.0, etc.).
     
     public init(name: String, copyright: String, license: License) {
         self.name = name
--- Sources/Acknowledgements/License.swift
+++ Sources/Acknowledgements/License.swift
@@ -9,13 +9,21 @@ import Foundation
 
 // MARK: Properties & Initializers
 
+/// A `License` that contains text.
+
 public struct License: Equatable {
     
     // MARK: Properties
     
+    /// The license text.
+    
     public let text: String
     
     // MARK: Initializers
+    
+    /// Initializes and returns a new `License`.
+    ///
+    /// - Parameter text: The license text.
     
     public init(text: String) {
         self.text = text
@@ -25,6 +33,9 @@ public struct License: Equatable {
 // MARK: - Licenses
 
 public extension License {
+    
+    /// MIT License
+    
     static let MIT = License(text:
         """
         Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -34,6 +45,8 @@ public extension License {
         THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
         """
     )
+    
+    /// Apache 2.0 License
     
     static let apache2 = License(text:
         """
